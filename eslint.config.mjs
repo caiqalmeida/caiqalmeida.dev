0e51129449a04426d07b8016199b80a41fdf629a
Caique Almeida <caaguima@gmail.com>
2025-03-15 13:54:03 -0300

    config: eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import pluginPrettier from "eslint-plugin-prettier";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +11,25 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  {
+    plugins: {
+      prettier: pluginPrettier,
+    },
+    rules: {
+      "prettier/prettier": [
+        "error",
+        {
+          bracketSameLine: false, 
+          printWidth: 100, 
+          jsxSingleQuote: false, 
+          trailingComma: "es5",
+        },
+      ],
+      "react/jsx-indent": ["error", 2],
+      "react/jsx-tag-spacing": ["error", { beforeSelfClosing: "always" }],
+    },
+  },
 ];
 
 export default eslintConfig;
